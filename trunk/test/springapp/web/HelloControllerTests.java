65b318aa372fdef4ed00017ce233019d9e0c1238
wrice127 <wrice127@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-09-20 18:05:01 +0000

    Add spring example. http://localhost:8080/proj4398/hello.spring

---
+++ trunk/test/springapp/web/HelloControllerTests.java
@@ -0,0 +1,19 @@
+package springapp.web;
+
+import org.springframework.web.servlet.ModelAndView;
+import springapp.web.HelloController;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HelloControllerTests {
+
+  @Test
+    public void testHandleRequestView() throws Exception{		
+        HelloController controller = new HelloController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);		
+        assertEquals("hello", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+        String nowValue = (String) modelAndView.getModel().get("now");
+        assertNotNull(nowValue);
+    }
+}
