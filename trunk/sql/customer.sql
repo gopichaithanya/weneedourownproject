d90c2ce368156e4f62c6365f51b80b8bdb86d957
israataha <israataha@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-09-29 18:40:30 +0000

    Added data to the Customer, Flight and Itinerary sql files

---
+++ trunk/sql/customer.sql
@@ -0,0 +1,20 @@
+/*************************************************************************/
+/**             SWE 642 - Software Engineering for the WWW              **/
+/**                             Fall 2008                               **/
+/**                                                                     **/
+/**                     Airline Reservation Project                     **/
+/**                                                                     **/
+/**                           Customer table	                        **/
+/*************************************************************************/;
+
+DROP TABLE customer;
+
+CREATE TABLE customer (first_name CHAR(20), last_name CHAR(20), 
+			street CHAR(50), city CHAR(20), state CHAR(2), zip INTEGER, 
+			username CHAR(20), password CHAR(20), 
+			cc_no INTEGER, expiration CHAR(5),
+			PRIMARY KEY(username));
+
+INSERT INTO customer VALUES (NULL,NULL,NULL,NULL,NULL,NULL,'administrator', 'tomcat', NULL, NULL);
+INSERT INTO customer VALUES ('Jack', 'Johnson', '1529 Main St', 'Fairfax', 'VA', 22030, 'jjohnson', 'harley', 8340384038493039, '08/10');
+INSERT INTO customer VALUES ('Samantha', 'Morris', '302 Cherry Hill Rd', 'Gaithersburg', 'MD', 20877, 'sammy', 'gmualum03', NULL, NULL);
