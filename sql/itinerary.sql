42a17f7968e06bc24956d07e536eb6238239f37f
israataha <israataha@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-09-29 18:40:30 +0000

    Added data to the Customer, Flight and Itinerary sql files

---
+++ sql/itinerary.sql
@@ -0,0 +1,20 @@
+/*************************************************************************/
+/**             SWE 642 - Software Engineering for the WWW              **/
+/**                             Fall 2008                               **/
+/**                                                                     **/
+/**                     Airline Reservation Project                     **/
+/**                                                                     **/
+/**                           Itinerary table                           **/
+/*************************************************************************/;
+
+DROP TABLE itinerary;
+
+CREATE TABLE itinerary(	flight_no INTEGER,
+			username CHAR(20),
+			status CHAR(20),
+			PRIMARY KEY(flight, username),
+			FOREIGN KEY flight_no REFERENCES flight,
+			FOREIGN KEY username REFERENCES customer);
+
+INSERT INTO itinerary VALUES (1069, 'jjohnson', 'booked');
+INSERT INTO itinerary VALUES (54, 'jjohnson', 'booked');
