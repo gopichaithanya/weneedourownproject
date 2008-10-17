7467b3c493c1c8ef7364f39d567f549a45d902bc
wrice127 <wrice127@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-10-17 18:59:57 +0000

    Making HSQL Standalone mode. You must change the file, build.properties, whose variable, db.url, should be jdbc:hsqldb:${db.location}

--- trunk/src/util/HibernateUtil.java
+++ trunk/src/util/HibernateUtil.java
@@ -5,21 +5,38 @@ import org.hibernate.cfg.*;
 
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory;
+	private static final SessionFactory sessionFactory;
 
-    static {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+	static {
+		final String attrHibernateURL = "hibernate.connection.url";
+		final String attrCatalinaBase = "catlina.base";
+		final String catalinaBase = System.getProperty(attrCatalinaBase);
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
+		try {
+			// Create the SessionFactory from hibernate.cfg.xml
+			final Configuration cfg = new Configuration().configure();
+
+			final String url = cfg.getProperty(attrHibernateURL);
+			final int idxCatalinaBase = url.indexOf("${" + attrCatalinaBase
+					+ "}");
+			if (idxCatalinaBase >= 0) {
+				final String newURL = url.substring(0, idxCatalinaBase)
+						+ catalinaBase
+						+ url.substring(idxCatalinaBase + 3
+								+ attrCatalinaBase.length());
+				cfg.setProperty(attrHibernateURL, newURL);
+			}
+
+			sessionFactory = cfg.buildSessionFactory();
+		} catch (Throwable ex) {
+			// Make sure you log the exception, as it might be swallowed
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 
 }
