fa0f5d97dfef62a28752fd06c41ea19585eb9e18
israataha <israataha@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-10-19 15:23:37 +0000

    Added login and searchFlight function skeletons

--- src/hibernate/manager/FlightManager.java
+++ src/hibernate/manager/FlightManager.java
@@ -1,25 +1,89 @@
 package hibernate.manager;
 
+import java.util.List;
+
 import hibernate.*;
 import hibernate.util.*;
 import org.hibernate.Session;
 
 public class FlightManager {
 	
-	private void createAndStoreFlight(int flightNo, String code) {
+	public void createAndStoreFlight(int flightNo, String code) {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		
 		Flight flight = new Flight();
-		//flight.setFlightNo(flightNo);
-		//flight.setAirlineCode(code);
+		flight.setFlightNo(flightNo);
+		
+		// to retrieve an airline object
+		/*Airline airline = new Airline();
+		session.load(airline, new String(code));
+		
+		flight.setAirline(airline);*/
+		
 		session.save(flight);
+		session.getTransaction().commit();
+	}
+	
+	/**
+	 * Search for a flight by flight number
+	 * @param flightNumber flight number
+	 * @return flight
+	 */
+	public Flight searchFlight(int flightNumber) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		Flight flight = new Flight();
+		session.load(flight, flightNumber);
+		
+		session.getTransaction().commit();
+		
+		return flight;
+	}
+	
+	public List listFlights() {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		List result = session.createQuery("from Flight").list();
+		session.getTransaction().commit();
+
+	    return result;
+	}
+	
+	public void login(String username, String password) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		// retrieve customer record from the databse
+		Customer customer = new Customer();
+		session.load(customer, new String(username));
+		
+		if (customer == null) {
+			// customer record does not exist in the database
+		} 
+		else {
+			//compare entered password to stored password
+			if (password.compareTo(customer.getPassword()) == 0) {
+				// login successful
+			}
+			else {
+				// login error: incorrect password
+			}
+		}
 		session.getTransaction().commit();
 	}
 	
 	public static void main (String[] args){
 		FlightManager mgr = new FlightManager();
 		mgr.createAndStoreFlight(924, "FL");
+		
+		List flights = mgr.listFlights();
+	    for (int i = 0; i < flights.size(); i++) {
+	    	Flight flight = (Flight) flights.get(i);
+	        System.out.println("Flight: " + flight.getFlightNo());
+	    }
+
 		HibernateUtil.getSessionFactory().close();
 	}
 }
