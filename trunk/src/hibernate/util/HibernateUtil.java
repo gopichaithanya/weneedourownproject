a1d32aa268775b766815406e7bfe6cefb071a1d5
wrice127 <wrice127@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-10-20 14:10:01 +0000

    Fixied minor bugs on HibernateUtil.java

--- trunk/src/hibernate/util/HibernateUtil.java
+++ trunk/src/hibernate/util/HibernateUtil.java
@@ -1,7 +1,5 @@
 package hibernate.util;
 
-import java.io.File;
-
 import org.hibernate.*;
 import org.hibernate.cfg.*;
 
@@ -9,24 +7,13 @@ public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
 
-    private final static String attrCatalinaBase = "catalina.base";
-    public final static String configFilenameForTest = "xml/hibernate.ant.cfg.xml";
-
     static {
 //		System.out.println(System.getProperty("user.dir")); // working directory
 //		System.out.println(System.getProperty("java.class.path", "."));
 
         try {
-            final File cfgFile = new File(configFilenameForTest);
-            final boolean bFile = cfgFile.exists();
-
-            Configuration cfg = null;
-
-            if (bFile && false == isThisForTomcat())
-                cfg = new Configuration().configure(cfgFile);
-            else
-                // Create the SessionFactory from hibernate.cfg.xml
-                cfg = new Configuration().configure();
+            // Create the SessionFactory from hibernate.cfg.xml
+            final Configuration cfg = new Configuration().configure();
 
             sessionFactory = cfg.buildSessionFactory();
         } catch (Throwable ex) {
@@ -38,11 +25,5 @@ public class HibernateUtil {
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
-    }
-
-    private static boolean isThisForTomcat() {
-        final String catalinaBase = System.getProperty(attrCatalinaBase, ".");
-        final boolean bTomcat = !(".".equals(catalinaBase));
-        return bTomcat;
     }
 }
