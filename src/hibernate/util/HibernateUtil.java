0726245567fa7990de3453aa0854df62bba724f7
wrice127 <wrice127@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-10-20 05:10:54 +0000

    Fixing bugs on HibernateUtil.

--- src/hibernate/util/HibernateUtil.java
+++ src/hibernate/util/HibernateUtil.java
@@ -7,50 +7,42 @@ import org.hibernate.cfg.*;
 
 public class HibernateUtil {
 
-	private static final SessionFactory sessionFactory;
+    private static final SessionFactory sessionFactory;
 
-	final static String attrHibernateURL = "hibernate.connection.url";
-	final static String attrCatalinaBase = "catlina.base";
-	final static String configFilenameForTest = "xml/hibernate.ant.cfg.xml";
+    private final static String attrCatalinaBase = "catalina.base";
+    public final static String configFilenameForTest = "xml/hibernate.ant.cfg.xml";
 
-	static {
-		//		System.out.println(System.getProperty("user.dir"));
-		//		System.out.println(System.getProperty("java.class.path", "."));
+    static {
+//		System.out.println(System.getProperty("user.dir")); // working directory
+//		System.out.println(System.getProperty("java.class.path", "."));
 
-		try {
-			// Create the SessionFactory from hibernate.cfg.xml
-			final File cfgFile = new File(configFilenameForTest);
-			final boolean bFile = cfgFile.exists();
-			final Configuration cfgObj = new Configuration();
-			final Configuration cfg = (bFile ? cfgObj.configure(cfgFile)
-					: cfgObj.configure());
+        try {
+            final File cfgFile = new File(configFilenameForTest);
+            final boolean bFile = cfgFile.exists();
 
-			final String url = cfg.getProperty(attrHibernateURL);
-			final String parsedUrl = getParsedUrl(url);
-			cfg.setProperty(attrHibernateURL, parsedUrl);
+            Configuration cfg = null;
 
-			sessionFactory = cfg.buildSessionFactory();
-		} catch (Throwable ex) {
-			// Make sure you log the exception, as it might be swallowed
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
+            if (bFile && false == isThisForTomcat())
+                cfg = new Configuration().configure(cfgFile);
+            else
+                // Create the SessionFactory from hibernate.cfg.xml
+                cfg = new Configuration().configure();
 
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
+            sessionFactory = cfg.buildSessionFactory();
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
 
-	private static String getParsedUrl(String url) {
-		final String catalinaBase = System.getProperty(attrCatalinaBase, ".");
-		final int idxCatalinaBase = url.indexOf("${" + attrCatalinaBase + "}");
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 
-		if (idxCatalinaBase >= 0)
-			url = url.substring(0, idxCatalinaBase)
-					+ catalinaBase
-					+ url.substring(idxCatalinaBase + 3
-							+ attrCatalinaBase.length());
-
-		return url;
-	}
+    private static boolean isThisForTomcat() {
+        final String catalinaBase = System.getProperty(attrCatalinaBase, ".");
+        final boolean bTomcat = !(".".equals(catalinaBase));
+        return bTomcat;
+    }
 }
