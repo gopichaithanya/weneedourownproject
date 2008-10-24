45b61b31ce2ed83c506fcabfe367733fd4350c30
wrice127 <wrice127@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-10-24 23:47:49 +0000

    Modifing the result table of searching flight

--- trunk/src/springapp/web/FlightSearchForCustomer.java
+++ trunk/src/springapp/web/FlightSearchForCustomer.java
@@ -4,106 +4,124 @@ import java.util.Date;
 
 public class FlightSearchForCustomer {
 
-   // TODO change from String to Ineger
+   private String departLocation;
+   private String arrivalLocation;
+
    private String departMonth = "1";
    private String departDay = "1";
-   private String departYear;
-   private String departHour;
-   private String departMin;
-   private String departLocation;
-   
-   private String arrivalMonth = "1";
-   private String arrivalDay = "1";
-   private String arrivalYear;
-   private String arrivalHour;
-   private String arrivalMin;
-   private String arrivalLocation;
-   
+   private String departYear = "2008";
+   private String departHour = "anytime";
+
+   private String searchingHourRange = "3";
    private String numPassengers = "1";
    private String tripType = "roundTrip";
-   
+
+   private String returningMonth = "1";
+   private String returningDay = "1";
+   private String returningYear;
+   private String returningHour;
+
    public void setDepartMonth(String departMonth) {
       this.departMonth = departMonth;
    }
+
    public String getDepartMonth() {
       return departMonth;
    }
+
    public void setDepartDay(String departDay) {
       this.departDay = departDay;
    }
+
    public String getDepartDay() {
       return departDay;
    }
+
    public void setDepartYear(String departYear) {
       this.departYear = departYear;
    }
+
    public String getDepartYear() {
       return departYear;
    }
+
    public void setDepartHour(String departHour) {
       this.departHour = departHour;
    }
+
    public String getDepartHour() {
       return departHour;
    }
-   public void setDepartMin(String departMin) {
-      this.departMin = departMin;
-   }
-   public String getDepartMin() {
-      return departMin;
-   }
+
    public void setDepartLocation(String departLocation) {
       this.departLocation = departLocation;
    }
+
    public String getDepartLocation() {
       return departLocation;
    }
-   public void setArrivalMonth(String arrivalMonth) {
-      this.arrivalMonth = arrivalMonth;
+
+   public void setReturningMonth(String arrivalMonth) {
+      this.returningMonth = arrivalMonth;
    }
-   public String getArrivalMonth() {
-      return arrivalMonth;
+
+   public String getReturningMonth() {
+      return returningMonth;
    }
-   public void setArrivalDay(String arrivalDay) {
-      this.arrivalDay = arrivalDay;
+
+   public void setReturningDay(String arrivalDay) {
+      this.returningDay = arrivalDay;
    }
-   public String getArrivalDay() {
-      return arrivalDay;
+
+   public String getReturningDay() {
+      return returningDay;
    }
-   public void setArrivalYear(String arrivalYear) {
-      this.arrivalYear = arrivalYear;
+
+   public void setReturningYear(String arrivalYear) {
+      this.returningYear = arrivalYear;
    }
-   public String getArrivalYear() {
-      return arrivalYear;
+
+   public String getReturningYear() {
+      return returningYear;
    }
-   public void setArrivalHour(String arrivalHour) {
-      this.arrivalHour = arrivalHour;
+
+   public void setReturningHour(String arrivalHour) {
+      this.returningHour = arrivalHour;
    }
-   public String getArrivalHour() {
-      return arrivalHour;
+
+   public String getReturningHour() {
+      return returningHour;
    }
-   public void setArrivalMin(String arrivalMin) {
-      this.arrivalMin = arrivalMin;
-   }
-   public String getArrivalMin() {
-      return arrivalMin;
-   }
+
    public void setArrivalLocation(String arrivalLocation) {
       this.arrivalLocation = arrivalLocation;
    }
+
    public String getArrivalLocation() {
       return arrivalLocation;
    }
+
    public void setNumPassengers(String nPassengers) {
       this.numPassengers = nPassengers;
    }
+
    public String getNumPassengers() {
       return numPassengers;
    }
+
    public void setTripType(String tripType) {
       this.tripType = tripType;
    }
+
    public String getTripType() {
       return tripType;
+   }
+
+   public void setSearchingHourRange(String searchingTimeRange) {
+      this.searchingHourRange = searchingTimeRange;
+   }
+
+   public String getSearchingHourRange() {
+      return searchingHourRange;
    }
 }
