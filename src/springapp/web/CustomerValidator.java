171adf94f06fdccaf4b8c9cb34adc472381f5140
adetola.adewodu <adetola.adewodu@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-10-26 14:28:27 +0000

    Added CustomerValidator

--- src/springapp/web/CustomerValidator.java
+++ src/springapp/web/CustomerValidator.java
@@ -9,16 +9,7 @@ import hibernate.Customer;
 
 public class CustomerValidator  implements Validator
 {
-	private String username;
-	private String firstName;
-	private String lastName;
-	private String street;
-	private String city;
-	private String state;
-	private Integer zip;
-	private String password;
-	private Integer ccNo;
-	private String expiration;
+
 	public boolean supports(Class aClass)
 	{
 		return aClass.equals(Customer.class);
@@ -35,16 +26,12 @@ public class CustomerValidator  implements Validator
 			errors.rejectValue("firstName", "error.invalid.firstName", "First Name is blank");
 		if (customer.getLastName().length()  == 0)
 			errors.rejectValue("lastName", "error.invalid.lastName", "LastName is blank");
-		if (customer.getStreet() == null)
+		if (customer.getStreet().length()  == 0)
 			errors.rejectValue("street", "error.invalid.street", "Street Name is blank");
 		if (customer.getCity().length()  == 0)
 			errors.rejectValue("city", "error.invalid.city", "City is blank");
 		if (customer.getState().length()  == 0)
 			errors.rejectValue("state", "error.invalid.state", "State is blank");
-		if (customer.getZip().toString().length() != 5)
-			errors.rejectValue("apr", "error.invalid.apr", "Zip is not ");
-		if (customer.getCcNo().toString().length()  != 16)
-			errors.rejectValue("ccNo", "error.invalid.ccNo", "Credit Card Number is not correct");
 		if (customer.getPassword().toString().length() < 6)
 			errors.rejectValue("password", "error.invalid.password", "Password must be at least 6 characters");
 		if (customer.getExpiration().length()  != 4)
