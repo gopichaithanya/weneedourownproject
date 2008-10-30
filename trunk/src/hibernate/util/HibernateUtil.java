e84bd6e53b2d2092fb85fc541140d61ac4044ce7
wrice127 <wrice127@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-10-30 18:45:04 +0000

    Fixing HibernateUtil in order to be safer.

--- trunk/src/hibernate/util/HibernateUtil.java
+++ trunk/src/hibernate/util/HibernateUtil.java
@@ -1,29 +1,52 @@
 package hibernate.util;
 
+import java.io.File;
+import java.util.regex.Pattern;
+
 import org.hibernate.*;
 import org.hibernate.cfg.*;
 
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory;
+   private static final SessionFactory sessionFactory;
 
-    static {
-//		System.out.println(System.getProperty("user.dir")); // working directory
-//		System.out.println(System.getProperty("java.class.path", "."));
+   static {
+      final String catalBase = findCatalinaBase();
 
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            final Configuration cfg = new Configuration().configure();
+      try {
+         // Create the SessionFactory from hibernate.cfg.xml
+         final Configuration cfg = new Configuration().configure();
+         final String url = cfg.getProperty("connection.url");
+         if (null != catalBase && null != url) {
+            final String url2 = url.replaceFirst("${catalina.base}", catalBase);
+            cfg.setProperty("connection.url", url2);
+         }
 
-            sessionFactory = cfg.buildSessionFactory();
-        } catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+         sessionFactory = cfg.buildSessionFactory();
+      } catch (Throwable ex) {
+         // Make sure you log the exception, as it might be swallowed
+         System.err.println("Initial SessionFactory creation failed." + ex);
+         throw new ExceptionInInitializerError(ex);
+      }
+   }
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
+   private static String findCatalinaBase() {
+      final String classPath = System.getProperty("java.class.path", ".");
+      final String sep = String.valueOf(File.pathSeparatorChar);
+
+      final String[] classPaths = Pattern.compile(String.valueOf(sep)).split(classPath);
+      for (final String cp : classPaths) { // cp will be "catalina.base/webapps/proj4398/WEB-INF/classes
+         final File fileWebInf = new File(cp).getParentFile();
+         if (null == fileWebInf)
+            continue;
+         if (false == fileWebInf.getName().equals("WEB-INF"))
+            continue;
+         return fileWebInf.getParentFile().getParentFile().getParentFile().getAbsolutePath();
+      }
+      return null;
+   }
+
+   public static SessionFactory getSessionFactory() {
+      return sessionFactory;
+   }
 }
---
+++ trunk/src/hibernate/util/HibernateUtilTest.java
@@ -0,0 +1,43 @@
+package hibernate.util;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HibernateUtilTest {
+
+   private final String expectedResult = System.getProperty("user.dir");
+
+   @Before
+   public void before() {
+      final String sep = String.valueOf(File.pathSeparatorChar);
+      final String delim = String.valueOf(File.separatorChar);
+
+      // catalina.base/webapps/proj4398/WEB-INF/classes
+      // .            /build  /war     /WEB-INF/classes
+      final String classesPath = expectedResult + delim + "build" + delim + "war" + delim
+            + "WEB-INF" + delim + "classes";
+      final String dummyClassPaths = delim + sep + classesPath + sep
+            + System.getProperty("java.class.path", ".");
+      System.setProperty("java.class.path", dummyClassPaths);
+   }
+
+   @Test
+   public void testFindDatabase() throws IOException, SecurityException, NoSuchMethodException,
+         IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+
+      final Method methodFindDatabase = HibernateUtil.class.getDeclaredMethod("findCatalinaBase");
+      assertNotNull(methodFindDatabase);
+      methodFindDatabase.setAccessible(true);
+      final String catalBase = (String) methodFindDatabase.invoke(null);
+      assertNotNull(catalBase);
+
+      assertEquals(expectedResult, catalBase);
+   }
+}
