ed913a4ccfba6fd702389fb433727b7e237ceb5e
wrice127 <wrice127@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-10-30 20:23:05 +0000

    Fixing login cookie problem.

--- src/springapp/web/LoginController.java
+++ src/springapp/web/LoginController.java
@@ -17,22 +17,26 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.springframework.web.servlet.view.RedirectView;
 
-
 public class LoginController extends SimpleFormController {
-	
-	 protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,
-	         Object command, BindException errors) throws Exception {
-		 Customer customer = (Customer)command;
-		 	
-	      final ModelAndView mv = new ModelAndView(new RedirectView(getSuccessView()));
-	      final List<String[]> airports = AirportManager.getAirportCodeAndName();
-	      mv.addObject("airports", airports);
-			Cookie c = new Cookie("username", customer.getUsername());
-			response.addCookie(c);
-	      //HttpSession session = request.getSession(true);
-	      //session.setAttribute("username", customer.getUsername());
-	      return mv;
 
-	   }
+   public static final String URL = "login.spring";
+
+   protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,
+         Object command, BindException errors) throws Exception {
+
+      final Customer customer = (Customer) command;
+      final HttpSession session = request.getSession();
+
+      final String afterLogin = (String) session
+            .getAttribute(SessionConstants.LOGIN_REDIRECT_AFTER_LOGIN);
+      final String successView = (null == afterLogin || afterLogin.length() <= 0) ? getSuccessView()
+            : afterLogin;
+      session.removeAttribute(SessionConstants.LOGIN_REDIRECT_AFTER_LOGIN);
+      final ModelAndView mv = new ModelAndView(new RedirectView(successView));
+
+      session.setAttribute(SessionConstants.USERNAME, customer.getUsername());
+      return mv;
+
+   }
 
 }
---
+++ src/springapp/web/SessionConstants.java
@@ -0,0 +1,8 @@
+package springapp.web;
+
+public abstract class SessionConstants {
+
+   public static final String USERNAME = "username";
+   public static final String FLIGHT_SEARCH_FOR_CUSTOMER_C_M_D = "flightSearchForCustomerCMD";
+   public static final String LOGIN_REDIRECT_AFTER_LOGIN = "login.redirectAfterLogin";
+}
