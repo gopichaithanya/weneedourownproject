82f9b28a3985bcdb7f29fde46ea59c956da79dc5
adetola.adewodu <adetola.adewodu@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-10-30 05:48:35 +0000

    I use a cookie instead of a session to save the username information. This will enable us to track the username of the customer and retrieve information for the database.

--- trunk/src/springapp/web/CustomerController.java
+++ trunk/src/springapp/web/CustomerController.java
@@ -3,12 +3,16 @@ package springapp.web;
 import org.hibernate.Session;
 import hibernate.util.HibernateUtil;
 import hibernate.manager.*;
-
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
+import org.springframework.web.servlet.view.RedirectView;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,12 +25,19 @@ import java.util.List;
 import javax.servlet.ServletContext;
 
 public class CustomerController extends SimpleFormController {
-	protected void doSubmitAction(Object o) throws ServletException, IOException
+	 protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,
+	         Object command, BindException errors) throws Exception 
 	{
-		Customer customer = (Customer)o;
+		 final ModelAndView mv = new ModelAndView(new RedirectView(getSuccessView()));
+		Customer customer = (Customer)command;
 		CustomerManager.register(customer);
-		HttpSession session = request.getSession(true);
-		session.setAttribute("username", username);
+		Cookie c = new Cookie("username", customer.getUsername());
+		response.addCookie(c);
+
+		//HttpSession session = request.getSession(true);
+		//session.setAttribute("username", customer.getUsername());
+		
+		return mv;
 		
 		
 	}
