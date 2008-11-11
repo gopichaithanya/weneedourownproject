b587558482e4d1c3f021a576e96a0436a1bcc6b8
israataha <israataha@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-11-11 01:39:44 +0000

    Added comments for javadoc

--- src/hibernate/Airport.java
+++ src/hibernate/Airport.java
@@ -11,28 +11,101 @@ import java.util.Set;
 @SuppressWarnings("serial")
 public class Airport  implements java.io.Serializable {
 
-	
+	/**
+	 * The airport code
+	 */
 	private String code;
+	
+	/**
+	 * The airport name
+	 */
     private String name;
+    
+    /**
+     * The airport city
+     */
     private String city;
+    
+    /**
+     * The airport county
+     */
     private String county;
+    
+    /**
+     * The airport state
+     */
     private String state;
+    
+    /**
+     * 
+     */
     private String latitude1;
+    
+    /**
+     * 
+     */
     private String latitude2;
+    
+    /**
+     * 
+     */
     private String longitude1;
+    
+    /**
+     * 
+     */
     private String longitude2;
+    
+    /**
+     * The airport elevation
+     */
     private String elevation;
+    
+    /**
+     * The airport telephone number
+     */
     private String telephone;
+    
+    /**
+     * The list of flights that depart from the specified departure location
+     */
     private Set<Flight> flightsForDepartureLocation = new HashSet<Flight>(0);
+    
+    /**
+     * The list of flights that arrive at a specified arrival location
+     */
     private Set<Flight> flightsForArrivalLocation = new HashSet<Flight>(0);
 
+    /**
+     * Constructs a new airport object
+     */
     public Airport() {
     }
 
-	
+	/**
+	 * Constructs a new airport object with the give airport code
+	 * @param code - the airport code
+	 */
     public Airport(String code) {
         this.code = code;
     }
+    
+    /**
+     * 
+     * @param code
+     * @param name
+     * @param city
+     * @param county
+     * @param state
+     * @param latitude1
+     * @param latitude2
+     * @param longitude1
+     * @param longitude2
+     * @param elevation
+     * @param telephone
+     * @param flightsForDepartureLocation
+     * @param flightsForArrivalLocation
+     */
     public Airport(String code, String name, String city, String county, String state, String latitude1, String latitude2, String longitude1, String longitude2, String elevation, String telephone, Set<Flight> flightsForDepartureLocation, Set<Flight> flightsForArrivalLocation) {
        this.code = code;
        this.name = name;
@@ -49,48 +122,106 @@ public class Airport  implements java.io.Serializable {
        this.flightsForArrivalLocation = flightsForArrivalLocation;
     }
    
+    /**
+     * Returns the airport code
+     * @return the airport code
+     */
     public String getCode() {
         return this.code;
     }
     
+    /**
+     * Sets the airport code
+     * @param code
+     */
     public void setCode(String code) {
         this.code = code;
     }
+    
+    /**
+     * Returns the airport name
+     * @return the airport name
+     */
     public String getName() {
         return this.name;
     }
     
+    /**
+     * Sets the airport name
+     * @param name - the airport name
+     */
     public void setName(String name) {
         this.name = name;
     }
+    
+    /**
+     * Returns the city in which the airport is located
+     * @return - the airport city 
+     */
     public String getCity() {
         return this.city;
     }
     
+    /**
+     * Sets the city in which the airport is located
+     * @param city - the airport city
+     */
     public void setCity(String city) {
         this.city = city;
     }
+    
+    /**
+     * Returns the county in which the airport is located
+     * @return the airport county
+     */
     public String getCounty() {
         return this.county;
     }
     
+    /**
+     * Sets the county in which the airport is located
+     * @param county - the airport county
+     */
     public void setCounty(String county) {
         this.county = county;
     }
+    
+    /**
+     * Returns the state in which the airport is located
+     * @return the airport state
+     */
     public String getState() {
         return this.state;
     }
     
+    /**
+     * Sets the state in which the airport is located
+     * @param state - the airport state
+     */
     public void setState(String state) {
         this.state = state;
     }
+    
+    /**
+     * Return the latitude of the airport location
+     * @return - latitude1
+     */
     public String getLatitude1() {
         return this.latitude1;
     }
     
+    /**
+     * Sets the latitude of the airport location
+     * @param latitude1 - the airport latitude
+     */
     public void setLatitude1(String latitude1) {
         this.latitude1 = latitude1;
     }
+    
+    /**
+     * 
+     * @return
+     */
     public String getLatitude2() {
         return this.latitude2;
     }
@@ -112,38 +243,70 @@ public class Airport  implements java.io.Serializable {
     public void setLongitude2(String longitude2) {
         this.longitude2 = longitude2;
     }
+    
+    /**
+     * Returns the airport's elevation
+     * @return the airport elevation
+     */
     public String getElevation() {
         return this.elevation;
     }
     
+    /**
+     * Sets the airport's elevation
+     * @param elevation - the airport elevation
+     */
     public void setElevation(String elevation) {
         this.elevation = elevation;
     }
+    
+    /**
+     * Returns the airport telephone number
+     * @return the airport telephone number
+     */
     public String getTelephone() {
         return this.telephone;
     }
     
+    /**
+     * Sets the airport telephone number
+     * @param telephone - the airport telephone number
+     */
     public void setTelephone(String telephone) {
         this.telephone = telephone;
     }
+    
+    /**
+     * Returns the list of flights that depart from a specified departure location
+     * @return the list of flights
+     */
     public Set<Flight> getFlightsForDepartureLocation() {
         return this.flightsForDepartureLocation;
     }
     
+    /**
+     * Sets the list of flights that depart from a specified departure location
+     * @param flightsForDepartureLocation - the list of flights
+     */
     public void setFlightsForDepartureLocation(Set<Flight> flightsForDepartureLocation) {
         this.flightsForDepartureLocation = flightsForDepartureLocation;
     }
+    
+    /**
+     * Returns the list of flights that arrive at a specified arrival location
+     * @return the list of flights
+     */
     public Set<Flight> getFlightsForArrivalLocation() {
         return this.flightsForArrivalLocation;
     }
     
+    /**
+     * Sets the list of flights that arrive at a specified arrival location
+     * @param flightsForArrivalLocation - the list of flights
+     */
     public void setFlightsForArrivalLocation(Set<Flight> flightsForArrivalLocation) {
         this.flightsForArrivalLocation = flightsForArrivalLocation;
     }
-
-
-
-
 }
 
 
