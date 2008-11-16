cf313889a9614e0c6e1224f13e7bd6288a1a029b
wrice127 <wrice127@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-11-16 03:17:48 +0000

    Add formBackingObject method on CreditCardController.java

--- src/springapp/web/CreditCardController.java
+++ src/springapp/web/CreditCardController.java
@@ -19,38 +19,50 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class CreditCardController extends SimpleFormController {
-	
-	/*public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-    	throws Exception {
-		
-		final HttpSession session = request.getSession();
-	    final String userName = LoginController.getUserName(session);
-	    Customer customer = CustomerManager.getCustomer(userName);
-	    
-		//final ModelAndView mv = new ModelAndView(this.getFormView());
-		final ModelAndView mv = new ModelAndView(new RedirectView(getSuccessView()));
-		
-		mv.addObject("customer", customer);
-		return mv;
-		
-	}*/
 
-	protected void doSubmitAction(Object command) 
-	{
-		Customer customer = (Customer)command;
-		//return new ModelAndView(new RedirectView(getSuccessView()));
-		
-	}
-	/*protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,
-			 	         Object command, BindException errors) throws Exception {
+//   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+//         throws Exception {
+//
+//      ModelAndView mv = null;
+//      final Object command = super.getCommand(request);
+//      if (null != command) {
+//         mv = super.handleRequest(request, response);
+//      } else {
+//         mv = new ModelAndView(this.getFormView());
+//      }
+//
+//      return mv;
+//
+//   }
 
-		System.out.println("CreditCardController");
-		
-		Customer customer = (Customer)command;
-		System.out.println("CCNo: " + customer.getCcNo());
-		System.out.println("Expiration: " + customer.getExpiration());
-		//CustomerManager.register(customer);
-		
-		return new ModelAndView(new RedirectView(getSuccessView()));
-	}*/
+   protected void doSubmitAction(Object command) {
+      Customer customer = (Customer) command;
+      //return new ModelAndView(new RedirectView(getSuccessView()));
+
+   }
+
+//   protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,
+//         Object command, BindException errors) throws Exception {
+//
+//      System.out.println("CreditCardController");
+//
+//      Customer customer = (Customer) command;
+//      System.out.println("CCNo: " + customer.getCcNo());
+//      System.out.println("Expiration: " + customer.getExpiration());
+//      //CustomerManager.register(customer);
+//
+//      return new ModelAndView(new RedirectView(getSuccessView()));
+//   }
+
+   @Override
+   protected Object formBackingObject(HttpServletRequest request) throws Exception {
+
+      final HttpSession session = request.getSession();
+      final String userName = LoginController.getUserName(session);
+//   Customer customer = CustomerManager.getCustomer(userName);
+      Customer customer = new Customer();
+      customer.setCcNo((long) 1111);
+      customer.setExpiration(111111);
+      return customer;
+   }
 }
