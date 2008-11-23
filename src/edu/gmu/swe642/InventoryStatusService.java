3f78dccdc109cd6d917aeba190daa2d53822aea2
adetola.adewodu <adetola.adewodu@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-11-23 16:07:51 +0000

    Commented out code in getNumOfEmptySeats function. It code works in the main but not in the web service.

--- src/edu/gmu/swe642/InventoryStatusService.java
+++ src/edu/gmu/swe642/InventoryStatusService.java
@@ -1,13 +1,9 @@
 package edu.gmu.swe642;
 
-import hibernate.Flight;
-import hibernate.manager.FlightManager;
-
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Inventory Status on number of available seats on flights
@@ -25,39 +21,35 @@ public class InventoryStatusService {
 	// yyyy.MMMMM.dd
 	public int getNumOfEmptySeats(String testdate) {
 
-		int flightDate = 0;
-		Date date = getDate(testdate);
+		int flightDate = 42;
+		// Date date = getDate(testdate);
 
-		System.out.println(date);
-
-		DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy/hh");
-		String s = formatter.format(date);
-		System.out.println("Today is " + s);
-
-		String[] dateParts = s.split("/");
-
-		System.out.println("The Month is " + dateParts[0]);
-		System.out.println("The date is " + dateParts[1]);
-		System.out.println("The Year is " + dateParts[2]);
-		System.out.println("The hour is " + dateParts[3]);
-
-		int month = Integer.parseInt(dateParts[0]);
-		int day = Integer.parseInt(dateParts[1]);
-		int year = Integer.parseInt(dateParts[2]);
-		int hour = Integer.parseInt(dateParts[3]);
-
-		final List flightList = FlightManager.getFlightList("IAD", "LAX", year,
-				month, day, hour, 2);
-
-		if (flightList.size() > 0) {
-			Flight flightTest = (Flight) flightList.get(0);
-			// testDate = flightTest.getDepartureTime();
-			System.out.println(flightTest.getBusinessSeats()
-					+ flightTest.getEconomySeats());
-			flightDate = flightTest.getBusinessSeats()
-					+ flightTest.getEconomySeats();
-
-		}
+		// try {
+		//
+		// DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy/hh");
+		// String s = formatter.format(date);
+		// System.out.println("Today is " + s);
+		//
+		// String[] dateParts = s.split("/");
+		//
+		// int month = Integer.parseInt(dateParts[0]);
+		// int day = Integer.parseInt(dateParts[1]);
+		// int year = Integer.parseInt(dateParts[2]);
+		// int hour = Integer.parseInt(dateParts[3]);
+		//
+		// final List flightList = FlightManager.getFlightList("IAD", "LAX",
+		// year, month, day, hour, 2);
+		//
+		// if (flightList.size() > 0) {
+		// Flight flightTest = (Flight) flightList.get(0);
+		// // testDate = flightTest.getDepartureTime();
+		// flightDate = flightTest.getBusinessSeats()
+		// + flightTest.getEconomySeats();
+		//
+		// }
+		// } catch (Exception e) {
+		// e.toString();
+		// }
 
 		return flightDate;
 	}
@@ -76,11 +68,11 @@ public class InventoryStatusService {
 	 * @return
 	 */
 	static public Date getDate(String test) {
-		DateFormat formatter = new SimpleDateFormat();
+
 		Date date = new Date();
 
 		try {
-
+			DateFormat formatter = new SimpleDateFormat();
 			test = test.replaceAll("-", "/");
 			test = test.replaceAll(" ", "/");
 
@@ -96,12 +88,12 @@ public class InventoryStatusService {
 
 	public static void main(String[] args) {
 		InventoryStatusService test = new InventoryStatusService();
-		int e = test.getNumOfEmptySeats("12-26-2008 10");
+		int e = test.getNumOfEmptySeats("12/26/2008/10");
 
-		System.out.println(e);
+		// System.out.println(e);
 		Date today = new Date();
 		DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy/hh");
-		System.out.println(formatter.format(today));
+		// System.out.println(formatter.format(today));
 
 	}
 }
