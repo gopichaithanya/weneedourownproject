80b443c11d88d1346b13bde8ae5acbadd854c58e
israataha <israataha@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-11-25 03:07:38 +0000

    Added comments to my controller classes in spring.web package

--- src/springapp/web/TicketController.java
+++ src/springapp/web/TicketController.java
@@ -10,7 +10,18 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
+/**
+ * TicketController is a Spring controller for the ticket page. It handles the creation of a ticket
+ * to be displayed to the user as a confirmation of booking and payment 
+ * 
+ * @author Israa Taha
+ */
 public class TicketController extends SimpleFormController {
+	
+	/**
+	 * Process the request and return a ModelAndView object which the DispatcherServlet will render.
+	 * @return the ModelAndView object for the ticket page
+	 */
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
 		final HttpSession session = request.getSession();
