329e9a9d49af04285a526ee2f26fcf4758793e63
wrice127 <wrice127@fc346fb0-8518-11dd-9899-c7ba65f7c4c7>
2008-11-26 22:12:26 +0000

    Fixing a bug on itineraryManager.

--- trunk/src/hibernate/manager/ItineraryManagerTest.java
+++ trunk/src/hibernate/manager/ItineraryManagerTest.java
@@ -16,6 +16,19 @@ public class ItineraryManagerTest {
       ItineraryManager.cancelReserved("jjohnson", 157);
       final boolean bReserve = ItineraryManager.reserve("jjohnson", 157, ESeatClass.ECONOMY, 1);
       assertTrue(bReserve);
+
+      final List<Itinerary> reserved = ItineraryManager.getReserved("jjohnson");
+      boolean bFound = false;
+      for (final Itinerary it : reserved) {
+         if (false == it.getCustomer().getUsername().equals("jjohnson"))
+            continue;
+         if (157 != it.getFlight().getFlightNo())
+            continue;
+         bFound = true;
+         assertEquals(1, it.getNumOfSeats());
+         assertEquals(ESeatClass.ECONOMY.toString(), it.getSeatClass());
+      }
+      assertTrue(bFound);
    }
 
    @Test
